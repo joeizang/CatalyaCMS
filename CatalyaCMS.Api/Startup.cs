e428dca9ee1b871b9364e2fa8338e491abc0bcec
Joseph Izang <josephizang@gmail.com>
2021-11-15 22:34:31 +0100

    upgrade framework version to dotnet 6 LTS up from 3.1 LTS

--- CatalyaCMS.Api/Startup.cs
+++ CatalyaCMS.Api/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using CatalyaCMS.Domain.DomainModels;
@@ -14,63 +13,62 @@ using Microsoft.Extensions.Hosting;
 
 namespace CatalyaCMS.Api
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<SiteDbContext>(options =>
-            {
-                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
-
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<SiteDbContext>();
-
-            services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, SiteDbContext>();
-
-            services.AddAuthentication()
-                .AddIdentityServerJwt();
-
-            services.AddScoped<BaseQuerySpecification<Article>, ArticleListQuerySpecification>();
-            services.AddScoped<BaseQuerySpecification<Article>, ArticleDetailQuerySpecification>();
-            services.AddScoped<IRepository<Article>, ArticleRepository>();
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseIdentityServer();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<SiteDbContext>(options =>
+      {
+        options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+      });
+
+      services.AddDefaultIdentityUI<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+          .AddEntityFrameworkStores<SiteDbContext>();
+
+      services.AddIdentityServer()
+          .AddApiAuthorization<ApplicationUser, SiteDbContext>();
+
+      services.AddAuthentication()
+          .AddIdentityServerJwt();
+
+      services.AddScoped<BaseQuerySpecification<Article>, ArticleListQuerySpecification>();
+      services.AddScoped<BaseQuerySpecification<Article>, ArticleDetailQuerySpecification>();
+      services.AddScoped<IRepository<Article>, ArticleRepository>();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseAuthentication();
+      app.UseIdentityServer();
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller}/{action=Index}/{id?}");
+        endpoints.MapRazorPages();
+      });
+    }
+  }
 }
--- CatalyaCMS.Infrastructure/Context/SiteDbContext.cs
+++ CatalyaCMS.Infrastructure/Context/SiteDbContext.cs
@@ -4,62 +4,62 @@ using System.Text;
 using Microsoft.EntityFrameworkCore;
 using CatalyaCMS.Domain.DomainModels;
 using CatalyaCMS.Infrastructure.EntityTypeConfiguration;
-using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
 
 namespace CatalyaCMS.Infrastructure.Context
 {
-    public class SiteDbContext : ApiAuthorizationDbContext<ApplicationUser>
+  public class SiteDbContext : ApiAuthorizationDbContext<ApplicationUser>
+  {
+
+    public SiteDbContext(
+        DbContextOptions options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
     {
 
-        public SiteDbContext(
-            DbContextOptions options,
-            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
-        {
-            
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-        }
-
-        public DbSet<Article> Articles { get; set; }
-
-        public DbSet<Asset> Assets { get; set; }
-
-        public DbSet<ArticleTags> ArticleTags { get; set; }
-
-        public DbSet<SiteUser> Authors { get; set; }
-
-        public DbSet<SiteRole> SiteRoles { get; set; }
-
-        public DbSet<Gallery> Galleries { get; set; }
-
-        public DbSet<Picture> Pictures { get; set; }
-
-        public DbSet<Tag> Tags { get; set; }
-
-        public DbSet<Comment> Comments { get; set; }
-
-        public DbSet<Opinion> Opinions { get; set; }
-
-        public DbSet<Commentator> Commentators { get; set; }
-
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfiguration(new ArticleTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new ArticleTagsTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new AuthorTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new GalleryTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new PictureTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new TagTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new CommentatorTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new AssetTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new OpinionTypeConfiguration());
-        }
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      base.OnConfiguring(optionsBuilder);
+    }
+
+    public DbSet<Article> Articles { get; set; }
+
+    public DbSet<Asset> Assets { get; set; }
+
+    public DbSet<ArticleTags> ArticleTags { get; set; }
+
+    public DbSet<SiteUser> Authors { get; set; }
+
+    public DbSet<SiteRole> SiteRoles { get; set; }
+
+    public DbSet<Gallery> Galleries { get; set; }
+
+    public DbSet<Picture> Pictures { get; set; }
+
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<Comment> Comments { get; set; }
+
+    public DbSet<Opinion> Opinions { get; set; }
+
+    public DbSet<Commentator> Commentators { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfiguration(new ArticleTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new ArticleTagsTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new AuthorTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new GalleryTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new PictureTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new TagTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new CommentatorTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new AssetTypeConfiguration());
+      modelBuilder.ApplyConfiguration(new OpinionTypeConfiguration());
+    }
+  }
 }
